ae1a1edf081fca9da4d8cef77997cda50d390142
D-pex <deepesh.vishwakarma.dev@gmail.com>
2026-03-12 12:33:00 +0530

    "Added Cors Policy "

--- LibraryProject.Web/Program.cs
+++ LibraryProject.Web/Program.cs
@@ -20,13 +20,22 @@ builder.Services
     .AddScoped<CategoryServices>()
     .AddScoped<BookIssueService>();
 
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection(); 
+
+app.UseCors(option =>
+{
+    option.AllowAnyHeader();
+    option.AllowAnyMethod();
+    option.AllowAnyOrigin();
+});
+
 
 var apiGroup = app.MapGroup("/api");
 
